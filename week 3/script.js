c519458c42959123b87c37cf16f0ac4f00ea579c
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-02-18 23:19:31 +0300

    14th Commit
    Added 3 challenges for FEB 18 2023

--- week 3/script.js
+++ week 3/script.js
@@ -118,14 +118,137 @@
 
 // // SOLUTION
 // function colorPatternTimes(cols) {
-//   for (const color of cols) {
-//     if (cols[color + 1] === color) {
-//       timeNeeded = (cols.length - 2) * 2 + cols.length - 2;
-//     } else timeNeeded = cols.length * 2 + 1 * (cols.length - 1);
-//   }
-//   return timeNeeded;
+//   return cols.length * 2 + cols.length - 1;
 // }
 // console.log(colorPatternTimes(["Red", "Blue", "Red", "Blue", "Red"]));
 // console.log(colorPatternTimes(["Blue"]));
 // console.log(colorPatternTimes(["Red", "Yellow", "Green", "Blue"]));
 // console.log(colorPatternTimes(["Blue", "Blue", "Blue", "Red", "Red", "Red"]));
+
+//
+//
+//
+//
+// FEB 18 2023
+//
+//
+//
+// QUESTION 1
+// Create two functions:
+
+// The first is isOdd() to check if a given number is odd using bitwise operator.
+// The second is isEven() to check if a given input is even using regular expressions.
+// Use of % operator is disallowed.
+
+// Examples
+// isOdd(3) ➞ "Yes"
+// // Use Bitwise Operator
+
+// isOdd(58) ➞ "No"
+// // Use Bitwise Operator
+
+// isEven("0") ➞ "Yes"
+// // Use Regular Expression
+
+// isEven("-99") ➞ "No"
+// // Use Regular Expression
+// Notes
+// Input will only be integers (positive/negative/zero).
+// For the second function, input will be numbers in string.
+
+// // SOLUTION
+// function isOdd(num) {
+//   if ((num ^ 1) == num + 1) {
+//     return "Even";
+//   } else return "Odd";
+// }
+
+// function isEven(num) {
+//   if (Number(num) % 2 === 0) {
+//     return "Even";
+//   } else return "Odd";
+// }
+
+// console.log(isOdd(3));
+// console.log(isOdd(58));
+
+// console.log(isEven("0"));
+// console.log(isEven("-99"));
+
+//
+//
+//
+// QUESTION 2
+// Create a function that converts a word to a bitstring and then to a boolean array based on the following criteria:
+
+// Locate the position of the letter in the English alphabet (from 1 to 26).
+// Odd positions will be represented as 1 and even positions will be represented as 0.
+// Convert the represented positions to boolean values, 1 for true and 0 for false.
+// Store the conversions into an array.
+// Examples
+// toBoolArray("deep") ➞ [false, true, true, false]
+// // deep converts to 0110
+// // d is the 4th alphabet - 0
+// // e is the 5th alphabet - 1
+// // e is the 5th alphabet - 1
+// // p is the 16th alphabet - 0
+
+// toBoolArray("loves") ➞ [false, true, false, true, true]
+
+// toBoolArray("tesh") ➞ [false, true, true, false]
+// Notes
+// The letter A is at position 1 and Z at 26.
+
+// // SOLUTION
+// function toBoolArray(word) {
+//   let outputBool = [];
+//   const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+//   for (const letter of word) {
+//     if (alphabet.indexOf(letter) % 2 === 0) {
+//       outputBool.push(true);
+//     } else if (alphabet.indexOf(letter) % 2 === 1) {
+//       outputBool.push(false);
+//     }
+//   }
+//   return outputBool;
+// }
+
+// console.log(toBoolArray("deep"));
+// console.log(toBoolArray("loves"));
+// console.log(toBoolArray("tesh"));
+
+//
+//
+//
+// QUESTION 3A number is said to be Disarium if the sum of its digits raised to their respective positions is the number itself.
+
+// Create a function that determines whether a number is a Disarium or not.
+
+// Examples
+// isDisarium(75) ➞ false
+// // 7^1 + 5^2 = 7 + 25 = 32
+
+// isDisarium(135) ➞ true
+// // 1^1 + 3^2 + 5^3 = 1 + 9 + 125 = 135
+
+// isDisarium(544) ➞ false
+// isDisarium(518) ➞ true
+// isDisarium(8) ➞ true
+// isDisarium(466) ➞ false
+
+// // SOLUTION
+// function isDisarium(num) {
+//   let total = 0;
+//   for (let i = 0; i < String(num).length; i++) {
+//     total += Number(String(num)[i]) ** (i + 1);
+//   }
+//   if (total === num) return true;
+//   else return false;
+// }
+
+// console.log(isDisarium(75));
+// console.log(isDisarium(135));
+// console.log(isDisarium(544));
+// console.log(isDisarium(518));
+// console.log(isDisarium(8));
+// console.log(isDisarium(466));
