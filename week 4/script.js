da5504c4ad1f7a3a06bd9b26e11fcb8b54575aa9
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-02-25 20:53:08 +0300

    19th commit
    Added 25th challenges

--- week 4/script.js
+++ week 4/script.js
@@ -114,22 +114,125 @@
 
 // sortArray([38, 57, 45, 18, 47, 39]) ➞ [18, 38, 39, 45, 47, 57]
 
-// SOLUTION
-function sortArray(arr) {
-  const n = arr.length;
-  let swapped;
-  do {
-    swapped = false;
-    for (let i = 0; i < n - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-        swapped = true;
-      }
-    }
-  } while (swapped);
-  return arr;
-}
+// // SOLUTION
+// function sortArray(arr) {
+//   const n = arr.length;
+//   let swapped;
+//   do {
+//     swapped = false;
+//     for (let i = 0; i < n - 1; i++) {
+//       if (arr[i] > arr[i + 1]) {
+//         [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+//         swapped = true;
+//       }
+//     }
+//   } while (swapped);
+//   return arr;
+// }
 
-console.log(sortArray([2, -5, 1, 4, 7, 8]));
-console.log(sortArray([23, 15, 34, 17, -28]));
-console.log(sortArray([38, 57, 45, 18, 47, 39]));
+// console.log(sortArray([2, -5, 1, 4, 7, 8]));
+// console.log(sortArray([23, 15, 34, 17, -28]));
+// console.log(sortArray([38, 57, 45, 18, 47, 39]));
+//
+//
+//
+//
+//
+// FEB 25 2023
+//
+//
+//
+// QUESTION
+// Write a function that inserts a white space between every instance of a lower character followed immediately by an upper character.
+
+// Examples
+// insertWhitespace("SheWalksToTheBeach") ➞ "She Walks To The Beach"
+
+// insertWhitespace("MarvinTalksTooMuch") ➞ "Marvin Talks Too Much"
+
+// insertWhitespace("TheGreatestUpsetInHistory") ➞ "The Greatest Upset In History"
+// Notes
+// Each word in the phrase will be at least two characters long.
+
+// // // SOLUTION
+// function insertWhitespace(str) {
+//   let outputStr = "";
+//   for (const letter of str) {
+//     if (letter === letter.toLowerCase()) outputStr += letter;
+//     else outputStr += " " + letter;
+//   }
+//   return outputStr;
+// }
+// console.log(insertWhitespace("SheWalksToTheBeach"));
+// console.log(insertWhitespace("MarvinTalksTooMuch"));
+// console.log(insertWhitespace("TheGreatestUpsetInHistory"));
+
+//
+//
+//
+// QUESTION 2
+// Given what is supposed to be typed and what is actually typed, write a function that returns the broken key(s). The function looks like:
+
+// findBrokenKeys(correct phrase, what you actually typed)
+// Examples
+// findBrokenKeys("happy birthday", "hawwy birthday") ➞ ["p"]
+
+// findBrokenKeys("starry night", "starrq light") ➞ ["y", "n"]
+
+// findBrokenKeys("beethoven", "affthoif5") ➞ ["b", "e", "v", "n"]
+
+// Notes
+// Broken keys should be ordered by when they first appear in the sentence.
+// Only one broken key per letter should be listed.
+// Letters will all be in lower case.
+
+// // SOLUTION
+// function findBrokenKeys(str1, str2) {
+//   let broken = [];
+//   for (let i = 0; i < str1.length; i++) {
+//     if (str1[i] !== str2[i] && !broken.includes(str1[i])) {
+//       broken.push(str1[i]);
+//     }
+//   }
+//   return broken;
+// }
+
+// console.log(findBrokenKeys("happy birthday", "hawwy birthday"));
+// console.log(findBrokenKeys("starry night", "starrq light"));
+// console.log(findBrokenKeys("beethoven", "affthoif5"));
+//
+//
+//
+// QUESTION 3
+// In each input array, every number repeats at least once, except for two. Write a function that returns the two unique numbers.
+
+// Examples
+// returnUnique([1, 9, 8, 8, 7, 6, 1, 6]) ➞ [9, 7]
+
+// returnUnique([5, 5, 2, 4, 4, 4, 9, 9, 9, 1]) ➞ [2, 1]
+
+// returnUnique([9, 5, 6, 8, 7, 7, 1, 1, 1, 1, 1, 9, 8]) ➞ [5, 6]
+
+// Notes
+// Keep the same ordering in the output.
+
+// // SOLUTION
+// function returnUnique(arr) {
+//   let output_arr = [];
+//   for (const val of arr) {
+//     let count = 0;
+//     for (const val2 of arr) {
+//       if (val === val2) {
+//         count += 1;
+//       }
+//     }
+//     if (count === 1) {
+//       output_arr.push(val);
+//     }
+//   }
+//   return output_arr;
+// }
+
+// console.log(returnUnique([1, 9, 8, 8, 7, 6, 1, 6]));
+// console.log(returnUnique([5, 5, 2, 4, 4, 4, 9, 9, 9, 1]));
+// console.log(returnUnique([9, 5, 6, 8, 7, 7, 1, 1, 1, 1, 1, 9, 8]));
