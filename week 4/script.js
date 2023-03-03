14b0ecccae66484443338a7ed63a4f7d5d9b47c6
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-03-03 19:50:12 +0300

    25th commit
    Added week 5 challenges file plus challenge 3 of March 02 2023, it was incomplete

--- week 4/script.js
+++ week 4/script.js
@@ -979,5 +979,34 @@
 //
 //
 // QUESTION 3
-//
-// SOLUTION
+// Given a word, create an object that stores the indexes of each letter in an array.
+
+// Make sure the letters are the keys.
+// Make sure the letters are symbols.
+// Make sure the indexes are stored in an array and those arrays are values.
+// Examples
+// mapLetters("dodo") ➞ { d: [0, 2], o: [1, 3] }
+
+// mapLetters("froggy") ➞ { f: [0], r: [1], o: [2], g: [3, 4], y: [5] }
+
+// mapLetters("grapes") ➞ { g: [0], r: [1], a: [2], p: [3], e: [4], s: [5] }
+// Notes
+// All strings given will be lowercase.
+
+// // SOLUTION
+// function mapLetters(word) {
+//   const obj = {};
+//   for (let i = 0; i < word.length; i++) {
+//     const letter = word[i];
+//     if (obj[letter] === undefined) {
+//       obj[letter] = [i];
+//     } else {
+//       obj[letter].push(i);
+//     }
+//   }
+//   return obj;
+// }
+
+// console.log(mapLetters("dododo"));
+// console.log(mapLetters("froggy"));
+// console.log(mapLetters("grapes"));
